519d03f370f1bee843b42a3c0697b7dfa1fd5a8f
thekidisalright <dev.alexandresilva@gmail.com>
2026-03-14 04:34:43 -0300

    atualiza layout da tela inicial para incluir link para player-screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="flex-1">
-        <View className="flex-1 bg-white p-4">
-          <Text
-            style={{ fontFamily: "Roboto_700Bold" }}
-            className="text-blue-500 text-4xl"
-          >
-            Echoes
-          </Text>
+      <SafeAreaView className="flex-1 bg-neutral-900">
+        <View className="flex-1 bg-neutral-900 p-4">
+          <Link href={{ pathname: "/player-screen" }}>
+            <Text
+              style={{ fontFamily: "Roboto_700Bold" }}
+              className="text-white text-4xl"
+            >
+              Echoes
+            </Text>
+          </Link>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
