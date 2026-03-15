696639d0b69e656694522a3da15ecf9c22327e31
thekidisalright <dev.alexandresilva@gmail.com>
2026-03-15 03:18:38 -0300

    refatora PlayerScreen para usar ProgressBars e simplifica a exibição de tempo

--- app/player-screen.tsx
+++ app/player-screen.tsx
@@ -1,5 +1,5 @@
+import ProgressBars from "@/components/ProgressBar";
 import { Ionicons } from "@expo/vector-icons";
-import { Slider } from "@miblanchard/react-native-slider";
 import {
   setAudioModeAsync,
   useAudioPlayer,
@@ -10,16 +10,6 @@ import { useEffect } from "react";
 import { Image, StatusBar, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 const audioSource = require("../assets/test/dualipa_troye.mp3");
-
-const formatTime = (timeInSeconds: number) => {
-  if (!timeInSeconds || isNaN(timeInSeconds)) return "00:00";
-
-  const totalSeconds = Math.floor(timeInSeconds);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
-
-  return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-};
 
 export default function PlayerScreen() {
   const player = useAudioPlayer(audioSource);
@@ -80,26 +70,7 @@ export default function PlayerScreen() {
               Chapter 1 of 12
             </Text>
           </View>
-          <View className="w-full px-4">
-            <Slider
-              minimumValue={0}
-              maximumValue={audioStatus.duration}
-              value={audioStatus.currentTime}
-              minimumTrackTintColor="#D4A017"
-              maximumTrackTintColor="#d4d4d4"
-              thumbTintColor="#D4A017"
-              trackStyle={{ height: 5, borderRadius: 3 }}
-              thumbStyle={{ width: 16, height: 16, borderRadius: 8 }}
-            />
-            <View className="flex-row justify-between">
-              <Text className="text-xs text-neutral-400 font-light">
-                {formatTime(audioStatus.currentTime)}
-              </Text>
-              <Text className="text-xs text-neutral-400 font-light">
-                -{formatTime(audioStatus.duration - audioStatus.currentTime)}
-              </Text>
-            </View>
-          </View>
+          <ProgressBars player={player} status={audioStatus} />
           <View className="flex-row items-center justify-center gap-8 px-4">
             <Ionicons name="play-skip-back" size={22} color={"#fafafa"} />
             <Ionicons name="play-back" size={32} color={"#fafafa"} />
@@ -113,13 +84,38 @@ export default function PlayerScreen() {
             <Ionicons name="play-forward" size={32} color={"#fafafa"} />
             <Ionicons name="play-skip-forward" size={22} color={"#fafafa"} />
           </View>
-          <View className="mt-10 mb-4 px-4 flex-row justify-between">
-            <Ionicons name="volume-high-outline" size={24} color={"#fafafa"} />
-            <Ionicons name="time-outline" size={24} color={"#fafafa"} />
-            <Ionicons name="repeat-outline" size={24} color={"#fafafa"} />
-            <Ionicons name="moon-outline" size={24} color={"#fafafa"} />
-            <Ionicons name="bookmark" size={24} color={"#D4A017"} />
-            <Ionicons name="list-outline" size={24} color={"#fafafa"} />
+          {/* Removemos o justify-between do pai */}
+          <View className="mt-10 mb-2 px-2 flex-row">
+            {/* flex-1: divide igualmente. items-center: centraliza o ícone. py-2: deixa o botão mais "alto" */}
+            <TouchableOpacity className="flex-1 items-center justify-center py-2">
+              <Ionicons
+                name="volume-high-outline"
+                size={24}
+                color={"#fafafa"}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity className="flex-1 items-center justify-center py-2">
+              <Ionicons name="time-outline" size={24} color={"#fafafa"} />
+            </TouchableOpacity>
+
+            <TouchableOpacity className="flex-1 items-center justify-center py-2">
+              <Ionicons name="repeat-outline" size={24} color={"#fafafa"} />
+            </TouchableOpacity>
+
+            <TouchableOpacity className="flex-1 items-center justify-center py-2">
+              <Ionicons name="moon-outline" size={24} color={"#fafafa"} />
+            </TouchableOpacity>
+
+            <TouchableOpacity className="flex-1 items-center justify-center py-2">
+              <Ionicons name="bookmark" size={24} color={"#D4A017"} />
+            </TouchableOpacity>
+
+            <Link href={{ pathname: "/chapters" }} asChild>
+              <TouchableOpacity className="flex-1 items-center justify-center py-2">
+                <Ionicons name="list-outline" size={24} color={"#fafafa"} />
+              </TouchableOpacity>
+            </Link>
           </View>
         </View>
       </SafeAreaView>
