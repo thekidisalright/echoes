c12997f98603adb31e7ada74eea19947d6229e4f
thekidisalright <dev.alexandresilva@gmail.com>
2026-03-17 02:18:52 -0300

    refatora BookItem para usar TouchableOpacity para lidar com longPress e atualiza layout para melhor renderização

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,6 @@ import BookItem from "@/components/BookItem";
 import ContinueListeningItem from "@/components/ContinueListeningItem";
 import MiniPlayer from "@/components/MiniPlayer";
 import ScanBooks from "@/components/ScanBooks";
-import { Link } from "expo-router";
 import { useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
@@ -13,11 +12,7 @@ export default function Index() {
     <SafeAreaProvider>
       <SafeAreaView className="flex-1 bg-neutral-800">
         <View className="px-4 mt-4 mb-5 flex-row justify-between items-center">
-          <Link href={{ pathname: "/player-screen" }}>
-            <Text className="text-white text-5xl font-cormorantbold">
-              Echoes
-            </Text>
-          </Link>
+          <Text className="text-white text-5xl font-cormorantbold">Echoes</Text>
           <ScanBooks setBooks={setBooks} books={books} />
         </View>
         <ScrollView
--- components/BookItem.tsx
+++ components/BookItem.tsx
@@ -1,5 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Image, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 interface BookType {
   title: string;
@@ -8,34 +9,45 @@ interface BookType {
 }
 
 export default function BookItem({ book }: { book: BookType }) {
+  const handleLongPress = () => {
+    console.log("longpress: " + book.title);
+  };
   return (
-    <View className="w-[48%] mb-4">
-      <View
-        className={
-          book.cover
-            ? "overflow-hidden aspect-[3/4] w-full rounded-xl mb-1"
-            : "flex-row justify-center items-center bg-neutral-700 opacity-50 aspect-[3/4] w-full rounded-xl mb-1"
-        }
+    <Link href={{ pathname: "/player-screen" }} asChild>
+      <TouchableOpacity
+        className="w-[48%] mb-4 rounded-xl"
+        onLongPress={handleLongPress}
+        activeOpacity={0.5}
       >
-        {book.cover ? (
-          <Image
-            source={{ uri: book.cover }}
-            className="w-full h-full"
-            resizeMode="cover"
-          />
-        ) : (
-          <Ionicons name="image-outline" color={"#737373"} size={48} />
-        )}
-      </View>
-      <Text className="text-white text-sm font-medium" numberOfLines={1}>
-        {book.title ? book.title : "Untitled"}
-      </Text>
-      <Text className="text-neutral-400 text-xs" numberOfLines={1}>
-        {book.author ? book.author : "Unknown"}
-      </Text>
-      <Text className="text-neutral-400 text-xs mt-0.5" numberOfLines={1}>
-        2h 20m
-      </Text>
-    </View>
+        <View className="w-full">
+          <View
+            className={
+              book.cover
+                ? "overflow-hidden aspect-[3/4] w-full rounded-xl mb-1"
+                : "flex-row justify-center items-center bg-neutral-700 opacity-50 aspect-[3/4] w-full rounded-xl mb-1"
+            }
+          >
+            {book.cover ? (
+              <Image
+                source={{ uri: book.cover }}
+                className="w-full h-full"
+                resizeMode="cover"
+              />
+            ) : (
+              <Ionicons name="image-outline" color={"#737373"} size={48} />
+            )}
+          </View>
+          <Text className="text-white text-sm font-medium" numberOfLines={1}>
+            {book.title ? book.title : "Untitled"}
+          </Text>
+          <Text className="text-neutral-400 text-xs" numberOfLines={1}>
+            {book.author ? book.author : "Unknown"}
+          </Text>
+          <Text className="text-neutral-400 text-xs mt-0.5" numberOfLines={1}>
+            2h 20m
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
 }
