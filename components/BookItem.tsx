a4a06ea0c72a91f51cf0b430f91eb8b047c19555
thekidisalright <dev.alexandresilva@gmail.com>
2026-03-24 01:05:02 -0300

    adiciona cálculo da duração total do livro e exibe no BookItem

--- components/BookItem.tsx
+++ components/BookItem.tsx
@@ -1,11 +1,46 @@
+import { useLibraryContext } from "@/contexts/LibraryContext";
 import { usePlayerContext } from "@/contexts/PlayerContext";
 import { BookType } from "@/types/AppTypes";
 import { Ionicons } from "@expo/vector-icons";
+import { createAudioPlayer } from "expo-audio";
 import { useRouter } from "expo-router";
+import { useEffect } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function BookItem({ book }: { book: BookType }) {
   const { setCurrentBook, saveProgress } = usePlayerContext();
+  const { updateBook } = useLibraryContext();
+  useEffect(() => {
+    const calculateTotalDuration = async () => {
+      if (book.totalDuration) return;
+
+      try {
+        let sumSeconds = 0;
+
+        for (const chapter of book.chapters) {
+          const duration = await new Promise<number>((resolve) => {
+            const tempPlayer = createAudioPlayer(chapter.uri);
+            const listener = tempPlayer.addListener(
+              "playbackStatusUpdate",
+              (status) => {
+                if (status.duration > 0) {
+                  listener.remove();
+                  resolve(status.duration);
+                  tempPlayer.release();
+                }
+              },
+            );
+          });
+          sumSeconds += duration;
+        }
+        await updateBook(book.id, { totalDuration: sumSeconds });
+      } catch (error) {
+        console.error("Error while trying to fetch the book duration: ", error);
+      }
+    };
+
+    calculateTotalDuration();
+  }, [book.totalDuration, book.id]);
   const router = useRouter();
   const handlePress = async () => {
     await saveProgress();
@@ -14,6 +49,15 @@ export default function BookItem({ book }: { book: BookType }) {
   };
   const handleLongPress = () => {
     console.log("longpress: " + book.title);
+  };
+  const formatTime = (timeInSeconds: number) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+
+    if (hours > 0) {
+      return `${hours}h ${minutes.toString().padStart(2, "0")}m`;
+    }
+    return `${minutes}m`;
   };
   return (
     <TouchableOpacity
@@ -47,7 +91,7 @@ export default function BookItem({ book }: { book: BookType }) {
           {book.author ? book.author : "Unknown"}
         </Text>
         <Text className="text-neutral-400 text-xs mt-0.5" numberOfLines={1}>
-          2h 20m
+          {book.totalDuration ? formatTime(book.totalDuration) : "Loading..."}
         </Text>
       </View>
     </TouchableOpacity>
